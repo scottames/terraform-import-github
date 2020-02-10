6a4cf8cecde986b5c7fb12d56ac00fe6b64a3c5b
Scott Ames <scott@ames.sh>
2020-02-09 18:05:26 -0800

    [feat] repos: optionally output to file (per repo)

--- repository/main.go
+++ repository/main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"os"
-
 	"text/template"
 
 	"github.com/anchorfree/github-terraform-exporters/pkg/repository"
@@ -22,6 +22,7 @@ func main() {
 	org := flag.String("org", "AnchorFree", "GitHub Organisation")
 	repoType := flag.String("type", "public", "Limit by repo type (public, private)")
 	fast := flag.Bool("fast", false, "Don't run per repo additional query, some parameters are not passed otherwise")
+	out := flag.String("out", "", "Path to output files to in the format: repository.<repo.name>.tf. If not specified output will be printed to standard out.")
 
 	flag.Parse()
 
@@ -51,9 +52,40 @@ func main() {
 
 	t := template.Must(template.ParseFiles(*tPath))
 	for repo := range repos {
-		err := t.Execute(os.Stdout, repo)
-		if err != nil {
-			panic(err)
+		var filename = fmt.Sprintf("%s/repository.%s.tf", *out, *repo.Name)
+
+		if len(*out) > 0 {
+
+			if _, err := os.Stat(*out); os.IsNotExist(err) {
+				err := os.Mkdir(*out, 0755)
+				if err != nil {
+					panic(err)
+				}
+			}
+
+			f, err := os.Create(filename)
+			if err != nil {
+				panic(err)
+			}
+
+			err = t.Execute(f, repo)
+			if err != nil {
+				panic(err)
+			}
+
+			err = f.Close()
+			if err != nil {
+				panic(err)
+			}
+
+		} else {
+
+			err := t.Execute(os.Stdout, repo)
+			if err != nil {
+				panic(err)
+			}
+
 		}
+
 	}
 }
